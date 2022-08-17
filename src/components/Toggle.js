1d341219d7879b15a590290d3ed360b198f3bc56
kalunge <muthomititus+github@gmail.com>
2022-08-17 10:26:50 +0300

    Update state based on color

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,7 +1,18 @@
-import React from "react";
+import React, { useState } from 'react';
 
 function Toggle() {
-  return <button>OFF</button>;
+  const [isOn, setIsOn] = useState(false);
+
+  const toggleOn = () => {
+    setIsOn((isOn) => !isOn);
+  };
+
+  const color = isOn ? 'white' : 'red';
+  return (
+    <button style={{ backgroundColor: color }} onClick={toggleOn}>
+      {isOn ? 'ON' : 'OFF'}
+    </button>
+  );
 }
 
 export default Toggle;
